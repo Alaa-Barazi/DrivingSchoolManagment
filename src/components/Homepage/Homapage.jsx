9130fc7d7fad1c0edf1924bd9b8e020dcc1b7a4a
Alaa-Barazi <127497841+Alaa-Barazi@users.noreply.github.com>
2023-12-07 18:27:38 +0200

    modified:   src/components/Homepage/Homapage.jsx

--- src/components/Homepage/Homapage.jsx
+++ src/components/Homepage/Homapage.jsx
@@ -4,7 +4,6 @@ import { Link } from "react-router-dom";
 export default function Homepage() {
   return (
     <main className={styles.homepage}>
-      <AppNav />
       <section>
         <h1>
           You travel the world.
